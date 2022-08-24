8783cff684e2f76be0e21e51475cb0856de92899
Leonardo Campos <leovilis@gmail.com>
2022-08-24 14:27:55 -0300

    MODIFICANDO-ESTILOS

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
-function cleanForm(){
-    let formulario = document.getElementById('form');
-    formulario.reset();
-}
-function redirectUrl(){
-    window.location.href = "index.html"
-}
-
-let boton = document.getElementById("enviar");
-boton.addEventListener("click", onClick);
-
-function onClick(event){
+function onClick (event) {
     event.preventDefault();
-    this.style.backgroundColor = "black"
     console.log("click...");
     console.log(event);
-
-    const mensaje= {
-        comercio: document.getElementById('comercio').value,
-        titular: document.getElementById('titular').value,
-        celular: document.getElementById('celular').value
+  
+  
+    const mensaje = {
+      comercio: document.getElementById('comercio').value,
+      titular: document.getElementById('titular').value,
+      celular: document.getElementById('celular').value
     }
     console.log(mensaje);
-
+  
+  
     fetch("https://jsonplaceholder.typicode.com/posts", {
-
+      method: "POST",
+      body: JSON.stringify(mensaje),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
     })
-}
+      .then((response) => response.json())
+      .then((json) => { 
+          console.log(json);
+          Swal.fire(
+              'Enviado',
+              'Gracias por tu comentario',
+              'success'
+          );
+          cleanForm();
+      })
+      .catch((err) => console.log(err));
+  
+  }
+  
+  
+  function cleanForm() {
+      let formulario = document.getElementById('form');    
+      formulario.reset();    
+  }
+  
+  let boton = document.getElementById("enviar");
+  boton.addEventListener("click", onClick);
+  
